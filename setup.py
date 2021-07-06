7e8a130a689676f1cd67c14fe44230a2f598c0fa
Rami Mosleh <engrbm87@gmail.com>
2021-07-06 15:22:55 +0300

    prepare for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-setup(name='notifications_android_tv',
-      version='0.1.0',
-      description='Notificaions for Android/Fire TVs',
-      url='https://github.com/engrbm87/notifications_android_tv',
-      author='Rami Mousleh',
-      author_email='engrbm87@gmail.com',
-      install_requires=['requests'],
-      license='MIT',
-      packages=['notifications_android_tv'],
-      zip_safe = False)
+setup(
+    name="notifications_android_tv",
+    version="0.1.0",
+    description="Notifications for Android/Fire TVs",
+    url="https://github.com/engrbm87/notifications_android_tv",
+    download_url="https://github.com/engrbm87/notifications_android_tv/archive/refs/tags/0.1.0.tar.gz",
+    author="Rami Mousleh",
+    author_email="engrbm87@gmail.com",
+    install_requires=["requests"],
+    license="MIT",
+    packages=["notifications_android_tv"],
+    keywords = ["android tv", "fire tv", "notifications"],
+    zip_safe=False,
+)

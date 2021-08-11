021df6fda544fac712decccda6b969e0aa7b3507
Rami Mosleh <engrbm87@gmail.com>
2021-08-11 12:59:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="notifications_android_tv",
-    version="0.1.2",
+    version="0.1.3",
     description="Notifications for Android/Fire TVs",
     url="https://github.com/engrbm87/notifications_android_tv",
-    download_url="https://github.com/engrbm87/notifications_android_tv/archive/refs/tags/0.1.2.tar.gz",
+    download_url="https://github.com/engrbm87/notifications_android_tv/archive/refs/tags/0.1.3.tar.gz",
     author="Rami Mousleh",
     author_email="engrbm87@gmail.com",
     install_requires=["requests"],
